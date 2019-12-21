d4c89a68af53369608a587da9def551ecf0d6938
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-21 18:15:16 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,7 +27,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.|\?|\!/).count {|w| w.size < 1}.size
+    self.split(/\.|\?|\!/).count {|w| w.size < 5}.size
   end
   
   #self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
